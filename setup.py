8671a0a801c61c9a250d06202035feede6b2ea5d
Michael Pihlblad <michael.pihlblad@bahnhof.se>
2024-02-25 17:03:18 +0100

    Added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from setuptools import setup, find_packages
+
+setup(
+    name = "flowerhub-huawei-monitor",
+    version = "0.1.0",
+    description = "Monitor Modbus TCP communication from Flowerhub to Huawei inverter",
+    packages = find_packages(),
+    install_requires = ['huawei-solar', 'pyshark', 'pymodbus', 'paho-mqtt', 'homeassistant-mqtt-binding'] 
+)
