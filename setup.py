9aa87d456261c1c0bcaaf889a52f74bf4525776d
MichaelPihlblad <61126625+MichaelPihlblad@users.noreply.github.com>
2024-04-01 21:47:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version = "0.1.0",
     description = "Monitor Modbus TCP communication from Flowerhub to Huawei inverter",
     packages = find_packages(),
-    install_requires = ['huawei-solar', 'pyshark', 'pymodbus', 'paho-mqtt', 'homeassistant-mqtt-binding'] 
+    install_requires = ['huawei-solar', 'pyshark', 'pymodbus', 'paho-mqtt', 'homeassistant-mqtt-binding==2.0.3'] 
 )
